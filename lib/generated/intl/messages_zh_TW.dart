2377c2ef36d739c2772157200fe74006b15c09ca
ForgerWise <forgerwise@gmail.com>
2025-01-23 18:54:27 +0900

    Fix some lauchurl bugs and add new feedback page

--- lib/generated/intl/messages_zh_TW.dart
+++ lib/generated/intl/messages_zh_TW.dart
@@ -30,6 +30,8 @@ class MessageLookup extends MessageLookupByLibrary {
   static String m3(selectedHour, selectedMinute) =>
       "選擇的時間: ${selectedHour}:${selectedMinute}";
 
+  static String m4(version) => "版本: ${version}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "about": MessageLookupByLibrary.simpleMessage("關於"),
@@ -39,6 +41,7 @@ class MessageLookup extends MessageLookupByLibrary {
             "這個應用程式是完全開源且免費使用的。如果您有任何建議或想要貢獻此專案，請隨時訪問 GitHub 儲存庫。"),
         "aboutContentHomepage":
             MessageLookupByLibrary.simpleMessage("如果您對我們的其他專案有興趣，歡迎造訪我們的官方網站。"),
+        "aboutFoodlist": MessageLookupByLibrary.simpleMessage("關於 FoodList"),
         "add": MessageLookupByLibrary.simpleMessage("新增"),
         "addCategory": MessageLookupByLibrary.simpleMessage("新增類別"),
         "addIngredients": MessageLookupByLibrary.simpleMessage("新增食材"),
@@ -47,6 +50,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "apple": MessageLookupByLibrary.simpleMessage("蘋果"),
         "bean": MessageLookupByLibrary.simpleMessage("豆類"),
         "beef": MessageLookupByLibrary.simpleMessage("牛肉"),
+        "bugReport": MessageLookupByLibrary.simpleMessage("錯誤報告"),
+        "bugReportOfFoodlist":
+            MessageLookupByLibrary.simpleMessage("FoodList 的錯誤報告"),
         "cancel": MessageLookupByLibrary.simpleMessage("取消"),
         "carrot": MessageLookupByLibrary.simpleMessage("紅蘿蔔"),
         "catOrSubcatIsDelError": MessageLookupByLibrary.simpleMessage(
@@ -63,6 +69,10 @@ class MessageLookup extends MessageLookupByLibrary {
         "confirmsubcategorydelete":
             MessageLookupByLibrary.simpleMessage("您確定要刪除此子類別嗎？"),
         "contactUs": MessageLookupByLibrary.simpleMessage("聯絡我們"),
+        "contributeCode": MessageLookupByLibrary.simpleMessage("貢獻程式碼"),
+        "contributeTranslation": MessageLookupByLibrary.simpleMessage("貢獻翻譯"),
+        "contributeTranslationOfFoodlist":
+            MessageLookupByLibrary.simpleMessage("貢獻 FoodList 的翻譯"),
         "edit": MessageLookupByLibrary.simpleMessage("編輯"),
         "editCategoriesNotSupportedHint":
             MessageLookupByLibrary.simpleMessage("編輯類別功能尚未支援！"),
@@ -104,12 +114,15 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("為什麼通知沒有正常運作？"),
         "faqWhyNotificationNotWorkAns": MessageLookupByLibrary.simpleMessage(
             "請確保您在裝置的設定中已啟用應用程式的通知。如果仍然無法正常運作，請檢查電池最佳化設定，並為該應用程式禁用電池最佳化。"),
+        "feedback": MessageLookupByLibrary.simpleMessage("回饋"),
         "fish": MessageLookupByLibrary.simpleMessage("魚類"),
         "foodlist": MessageLookupByLibrary.simpleMessage("FoodList"),
         "foodlistExpiryNotification":
             MessageLookupByLibrary.simpleMessage("FoodList 有效日期通知"),
         "foodlistExpiryNotificationContent": m1,
         "forgerwise": MessageLookupByLibrary.simpleMessage("ForgerWise"),
+        "forgerwisesGithub":
+            MessageLookupByLibrary.simpleMessage("ForgerWise 的 GitHub"),
         "fruit": MessageLookupByLibrary.simpleMessage("水果"),
         "githubRepository": MessageLookupByLibrary.simpleMessage("GitHub 儲存庫"),
         "home": MessageLookupByLibrary.simpleMessage("首頁"),
@@ -118,6 +131,12 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("此語言尚未支援！但我們預計會在未來支援此語言！"),
         "languages": MessageLookupByLibrary.simpleMessage("語言"),
         "meat": MessageLookupByLibrary.simpleMessage("肉類"),
+        "mesOfBugReport": MessageLookupByLibrary.simpleMessage(
+            "錯誤: \n\n裝置: \n\n作業系統: \n\n應用程式版本: \n\n重現錯誤的流程: \n\n*如果您有螢幕截圖，請一併附上。\n\n*如果您不清楚您的裝置詳細資訊，可以跳過該部分不填寫。"),
+        "mesOfContributeTrans": MessageLookupByLibrary.simpleMessage(
+            "我想貢獻的語言: \n\n*如果您想貢獻一項新語言，我們將提供翻譯檔案。"),
+        "mesOfTransError": MessageLookupByLibrary.simpleMessage(
+            "語言: \n\n錯誤的翻譯: \n\n正確的翻譯: \n\n"),
         "milk": MessageLookupByLibrary.simpleMessage("牛奶"),
         "mushroom": MessageLookupByLibrary.simpleMessage("菇類"),
         "none": MessageLookupByLibrary.simpleMessage("無"),
@@ -128,6 +147,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "notificationMoreItems": m2,
         "notificationSetting": MessageLookupByLibrary.simpleMessage("通知設定"),
         "notifications": MessageLookupByLibrary.simpleMessage("通知"),
+        "officialWebsite": MessageLookupByLibrary.simpleMessage("官方網站"),
         "other": MessageLookupByLibrary.simpleMessage("其他"),
         "otherBeans": MessageLookupByLibrary.simpleMessage("其他豆類"),
         "otherEggMilk": MessageLookupByLibrary.simpleMessage("其他蛋奶"),
@@ -145,6 +165,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "privacyContent": MessageLookupByLibrary.simpleMessage(
             "本隱私政策說明我們的應用程式（以下簡稱為“應用程式”）如何收集、使用及披露您的資訊。應用程式致力於維護用戶的隱私權。我們的隱私政策旨在幫助您了解我們如何收集及使用您決定分享的個人資訊，並幫助您在使用應用程式時做出明智的決定。\n\n使用或訪問應用程式即表示您接受本隱私政策中描述的做法。如果您不同意此政策，請勿使用應用程式。我們保留隨時修改此政策的權利，請經常查看更新。您繼續使用應用程式即表示接受我們修改後的隱私政策。\n\n1. 我們收集的數據\n目前，應用程式不會收集任何個人數據。應用程式的設計是將與您的食物相關的所有數據儲存在裝置內，並且沒有任何功能可用來收集您的個人數據。\n\n2. 數據儲存與保護\n您的數據儲存在您的裝置中，除非由您或裝置提供商分享，否則其他人無法存取。\n\n3. 未來更新\n未來若有允許用戶上傳數據以進行同步的功能，我們仍不會將此資訊透露給第三方。我們將在這些功能新增時公告隱私政策的更新。\n\n4. 隱私政策變更\n應用程式保留隨時更改本政策及服務條款的權利。我們將通過向您帳戶中指定的主要電子郵件地址發送通知或在我們的網站上放置醒目的通知來告知您對隱私政策的重大更改。重大更改將在通知後30天生效。非重大更改或澄清將立即生效。您應定期檢查網站和本隱私頁面以了解更新。"),
         "processedfood": MessageLookupByLibrary.simpleMessage("加工食品"),
+        "rateThisApp": MessageLookupByLibrary.simpleMessage("為此應用程式評分"),
         "reset": MessageLookupByLibrary.simpleMessage("重置"),
         "salmon": MessageLookupByLibrary.simpleMessage("鮭魚"),
         "save": MessageLookupByLibrary.simpleMessage("儲存"),
@@ -159,7 +180,13 @@ class MessageLookup extends MessageLookupByLibrary {
         "subcategoryName": MessageLookupByLibrary.simpleMessage("子類別名稱"),
         "subcategoryNameInputHint":
             MessageLookupByLibrary.simpleMessage("請輸入子類別名稱"),
+        "translationError": MessageLookupByLibrary.simpleMessage("翻譯錯誤"),
+        "translationErrorOfFoodlist":
+            MessageLookupByLibrary.simpleMessage("FoodList 的翻譯錯誤"),
         "tuna": MessageLookupByLibrary.simpleMessage("鮪魚"),
-        "vegetable": MessageLookupByLibrary.simpleMessage("蔬菜")
+        "urlCopiedToClipboard":
+            MessageLookupByLibrary.simpleMessage("網址已複製到剪貼簿"),
+        "vegetable": MessageLookupByLibrary.simpleMessage("蔬菜"),
+        "versionVersion": m4
       };
 }
